e56ae1e570b62d3410b79070f5d9b2103636bdd5
icedvigilante <wm.ryan.mooney@gmail.com>
2021-08-22 13:24:19 -0800

    fix automigrate

--- backend/database/dbSetup.go
+++ backend/database/dbSetup.go
@@ -2,8 +2,7 @@ package database
 
 import (
 	"github.com/simpleittools/simplehome/models"
-	"gorm.io/driver/sqlite"
-	//"gorm.io/driver/postgres"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -11,11 +10,11 @@ var DB *gorm.DB
 
 func Conn() {
 	//Postgres
-	//dsn := "host=localhost user=home_admin password=mONK!ion48 dbname=rtl port=5432 sslmode=disable"
-	//conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := "host=localhost user=home_admin password=mONK!ion48 dbname=simplehomedb port=5432 sslmode=disable"
+	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	// SQLite (would need to replace postgres driver with "gorm.io/driver/sqlite")
-	conn, err := gorm.Open(sqlite.Open("simplehome.db"), &gorm.Config{})
+	//conn, err := gorm.Open(sqlite.Open("simplehome.db"), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connect to the DB")
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,15 +1,17 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
-	//ID			uint			`json:"id"`
-	gorm.Model
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email" gorm:"unique"`
-	Password  []byte `json:"-"`
-	//CreatedAt	time.Time		`json:"created_at"`
-	//UpdatedAt	time.Time		`json:"updated_at"`
-	//DeletedAt 	gorm.DeletedAt	`json:"deleted_at"`
+	ID        uint           `json:"id"`
+	FirstName string         `json:"first_name"`
+	LastName  string         `json:"last_name"`
+	Email     string         `json:"email" gorm:"unique"`
+	Password  []byte         `json:"-"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
